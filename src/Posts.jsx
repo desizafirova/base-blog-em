8ad4b712ce5170c413668a032c880b9a0de26a6b
DesiZafirova <desszafirowa@gmail.com>
2024-12-23 22:59:54 +0200

    handling Loading and Error States

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-import { fetchPosts, deletePost, updatePost } from "./api";
-import { PostDetail } from "./PostDetail";
+import { fetchPosts, deletePost, updatePost } from './api';
+import { PostDetail } from './PostDetail';
+import { useQuery } from '@tanstack/react-query';
 const maxPostPage = 10;
 
 export function Posts() {
@@ -9,7 +10,26 @@ export function Posts() {
   const [selectedPost, setSelectedPost] = useState(null);
 
   // replace with useQuery
-  const data = [];
+  const { data, isError, error, isLoading } = useQuery({
+    //  We add some options to the useQuery in order to let the query knows which data to fetch. useQuery takes an object of options. The order of the options doesn't matter
+    // The queryKey is what defines this data within the query cache and it is always an array
+    queryKey: ['posts'],
+    // this is the function that we are going to run when we fetch the data
+    queryFn: fetchPosts,
+  });
+
+  if (isLoading) {
+    return <h3>Loading...</h3>;
+  }
+
+  if (isError) {
+    return (
+      <>
+        <h3>Oops, something went wrong</h3>
+        <p>{error.toString()}</p>
+      </>
+    );
+  }
 
   return (
     <>
